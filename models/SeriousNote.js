43f573bba3e0d1ab3c3986b43b6bb0af559f5d68
samhamm <samhamm@gmail.com>
2015-02-25 21:02:39 -0800

    new schema; router adjusted accordingly; updated README

--- models/SeriousNote.js
+++ models/SeriousNote.js
@@ -3,10 +3,13 @@
 var mongoose = require('mongoose');
 
 var seriousNoteSchema = new mongoose.Schema({
-	noteID: String,
-	noteText: String,
-	noteAudio: String,
-	deliveryMethod: String
+	reminderID: Number,
+  userID: Number,
+  textContent: String,
+  mediaType: Number,
+  mediaContent: Buffer,
+  recipientID: Number,
+  messageType: Number,
 });
 
 module.exports = mongoose.model('SeriousNote', seriousNoteSchema);
